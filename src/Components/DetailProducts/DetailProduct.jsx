408bb57e7dfd4be0a6528bdd0a99a9042703785a
yonaswondmagegn <yonaswondmagegn954@gmail.com>
2024-05-26 15:46:53 +0300

    demo#1

--- src/Components/DetailProducts/DetailProduct.jsx
+++ src/Components/DetailProducts/DetailProduct.jsx
@@ -7,7 +7,7 @@ import AdminContact from './AdminContact'
 import './DetailCss/detailproduct.css'
 import CartIcon from '../Cart/CartIcon'
 import config from '../../../config'
-import backIcon from '../../assets/icons.svg'
+
 import { ArrowLeft } from 'react-feather'
 import { useNavigate } from 'react-router-dom'
 
